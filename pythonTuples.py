be2de61efe9d3573c3e606b38bb807fb11ca1c63
David Tran <tran.a.david@gmail.com>
2019-10-28 20:24:37 -0400

    Python Tuples

---
+++ pythonTuples.py
@@ -0,0 +1,12 @@
+#Tuples are immutable, meaning they can't be changed. Once an element is assigned, you can't change it
+#Tuples using ()
+
+t = (1,2,3)
+
+my_tuple = ('a', 'b', 'a', 'c')
+
+#Tuples have a count function where you can count how many of that object there is .count, .index('a') while find the first element of that object
+
+print(my_tuple.count('a'))
+
+#Tuples keep data integrity, which assigning values by accident harder to do
